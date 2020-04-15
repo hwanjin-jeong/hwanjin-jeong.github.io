26b9fdfd06fa150e64796df7ee89ad186441ffcd
Hwanjin Jeong <hwanjin.jeong@gmail.com>
2020-04-15 20:13:38 +0900

    remove unused package

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,12 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
 module.exports = {
   plugins: [
     `gatsby-plugin-typescript`,
+    `gatsby-transformer-remark`,
+    `gatsby-plugin-slug`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
-        name: 'posts',
         path: `${__dirname}/posts`,
-      },
-    },
-    `gatsby-transformer-remark`,    
-    `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/static/images`,
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
       },
     },
   ]
